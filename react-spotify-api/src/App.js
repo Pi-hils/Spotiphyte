4e36a0d36577b338d4f7ca187bdd0fa105179679
Hilda Amponsah <hilda.amp@hotmail.com>
2020-09-15 16:51:51 +0100

    spotify-react

--- react-spotify-api/src/App.js
+++ react-spotify-api/src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
     }
     return hashParams;
   }
+
   render(){
     return (
       <div className="App">
@@ -37,6 +38,7 @@ class App extends Component {
    </div>
   );
  }
-}
+};
+
 
 export default App;

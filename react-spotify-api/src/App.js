c10cd34f7a02b467070855f6452e61315a887a0b
Hilda Amponsah <hilda.amp@hotmail.com>
2020-09-15 16:34:11 +0100

    class added to App.js but broke

--- react-spotify-api/src/App.js
+++ react-spotify-api/src/App.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <a href='http://localhost:8888'>
-       <button>Login with Spotify</button>
+class App extends Component {
+  constructor(){
+    super();
+    const params = this.getHarshParams();
+    this.state ={
+      loggedIn: params.access_token ? true : false,
+      nowPlaying: {
+        name: 'Not Checked',
+        image: ''
+      }
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  render(){
+    return (
+      <div className="App">
+        <a href='http://localhost:8888'>
+         <button>Login with Spotify</button>
        </a>
+       <div>
+       Now Playing: { this.state.nowPlaying.name }
+       </div>
+     <div>
+      <img src={ this.state.nowPlaying.name } style= {{ width: 100}}/>
     </div>
+   </div>
   );
+ }
 }
 
 export default App;
